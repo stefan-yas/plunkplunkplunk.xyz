62e5252149c6b61d36b15d8e58d4482dea895053
stefanyas <stefan.sy.yas@gmail.com>
2021-11-15 11:27:40 +0100

    first commit

---
+++ public/prnt.sc-lottery/lottery.js
@@ -0,0 +1,23 @@
+const characters = "abcdefghijklmnopqrstuvwxyz0123456789";
+var input = document.getElementById("random");
+var linkResult = "prnt.sc/" + generateString(6);
+
+function generateString(length) {
+    let result = "";
+    const charactersLength = characters.length;
+    for (let i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+};
+
+function reGen() {
+    var inputField = document.getElementById("random");
+    var linkResult = "prnt.sc/" + generateString(6);
+    inputField.value = linkResult;
+}
+
+function urlDetermine() {
+    var url = document.getElementById('random').value
+    window.open(url);
+}
