49d2a4424ec936beda92d578fb19c82fd55d32db
stefanyas <stefan.sy.yas@gmail.com>
2022-01-07 21:37:14 +0100

    working bpm

--- public/bpm-tapper/tapper.js
+++ public/bpm-tapper/tapper.js
@@ -20,7 +20,7 @@ function logKey() {
     //console.log(lastWritten);
     time1 = Date.now();
     //console.log(time1);
-    document.getElementById("sample1").textContent = time1;
+    //document.getElementById("sample1").textContent = time1;
 
   } else if (lastWritten === "sample1") {
 
@@ -28,11 +28,11 @@ function logKey() {
     //console.log(lastWritten);
     time2 = Date.now();
     //console.log(time2);
-    document.getElementById("sample2").textContent = time2;
+    //document.getElementById("sample2").textContent = time2;
 
     interval1 = time2 - time1;
     //console.log(interval1);
-    document.getElementById("interval1").textContent = interval1;
+    //document.getElementById("interval1").textContent = interval1;
 
   } else if (lastWritten === "sample2") {
 
@@ -40,11 +40,11 @@ function logKey() {
     //console.log(lastWritten);
     time3 = Date.now();
     //console.log(time3);
-    document.getElementById("sample3").textContent = time3;
+    //document.getElementById("sample3").textContent = time3;
 
     interval2 = time3 - time2;
     //console.log(interval2);
-    document.getElementById("interval2").textContent = interval2;
+    //document.getElementById("interval2").textContent = interval2;
 
   } else if (lastWritten === "sample3") {
 
@@ -52,22 +52,22 @@ function logKey() {
     //console.log(lastWritten);
     time4 = Date.now();
     //console.log(time4);
-    document.getElementById("sample4").textContent = time4;
+    //document.getElementById("sample4").textContent = time4;
 
     interval3 = time4 - time3;
     //console.log(interval3);
-    document.getElementById("interval3").textContent = interval3;
+    //document.getElementById("interval3").textContent = interval3;
 
   } else if (lastWritten === "sample4") {
     lastWritten = "sample5";
     //console.log(lastWritten);
     time5 = Date.now();
     //console.log(time5);
-    document.getElementById("sample5").textContent = time5;
+    //document.getElementById("sample5").textContent = time5;
 
     interval4 = time5 - time4;
     //console.log(interval4);
-    document.getElementById("interval4").textContent = interval4;
+    //document.getElementById("interval4").textContent = interval4;
 
   } else if (lastWritten === "sample5") {
 
@@ -75,11 +75,11 @@ function logKey() {
     //console.log(lastWritten);
     time1 = Date.now();
     //console.log(time1);
-    document.getElementById("sample1").textContent = time1;
+    //document.getElementById("sample1").textContent = time1;
 
     interval0 = time1 - time5;
     //console.log(interval0);
-    document.getElementById("interval0").textContent = interval0;
+    //document.getElementById("interval0").textContent = interval0;
 
   } else {}
 
@@ -88,9 +88,9 @@ function logKey() {
 
 function updateAverage() {
   if (interval0) {
-    var averageMS = document.getElementById("averageMS").textContent = (interval1 + interval2 + interval3 + interval4 + interval0) / 5;
-    document.getElementById("averageS").textContent = averageMS / 1000;
-    var bpm = document.getElementById("bpm").textContent = 60 / averageMS * 1000;
-    document.getElementById("bps").textContent = bpm / 60;
+    var averageMS = (interval1 + interval2 + interval3 + interval4 + interval0) / 5;
+    //document.getElementById("averageS").textContent = averageMS / 1000;
+    var bpm = document.getElementById("BPM").textContent = (Math.round(60 / averageMS * 1000) + " BPM");
+    //document.getElementById("bps").textContent = bpm / 60;
   } else {}
 }
