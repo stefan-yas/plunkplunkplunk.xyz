09a06c40b4743209647fc5d2b9c4287182b30ed9
Eva <eva.cerni1@gmail.com>
2022-09-30 15:19:08 +0200

    fix: eyes that follow mouse

--- behold.js
+++ behold.js
@@ -1,9 +1,7 @@
-// The only thing you need to change here are the parts under Settings.
-
 // Settings:
 const EYE_IDS = ["leftEye", "rightEye"]; // Add IDs of each eye into this array
-const PROJECTION = 0.03 // default: 0.03 Drag/projection effect - decrease number to increase it or set to 1 to remove it (% between base center and mouse position)
-const DELAY = 100; // default: 100
+const PROJECTION = 0.02 // Drag/projection effect - decrease number to increase it or set to 1 to remove it (% between base center and mouse position)
+const DELAY = 100;
 
 window.onload = () => {
 
@@ -12,10 +10,13 @@ window.onload = () => {
     EYE_IDS.forEach(e => {
         const container = document.getElementById(e);
         const baseElement = container.querySelector(".eye-base");
-        const dotElement = baseElement.querySelector(".eye-dot");
-        renderEye(baseElement, dotElement);
-    })
+        const dotElement = baseElement.querySelector(".eye-dot");        
+        renderEye(baseElement, dotElement);          
+    });
+
 }
+
+//+ window.pageYOffset;
 
 function renderEye(baseEl, dotEl) {
     // Things in this section run after the page loaded
@@ -26,22 +27,19 @@ function renderEye(baseEl, dotEl) {
     const dotRadius = dotBox.height / 2;
     const r = baseRadius - dotRadius + 1; // moved inwards by the side of eye dot, but minus 1px (which makes it look smoother)
 
-    console.log(baseRadius, dotRadius);
-
     // Find center of the base element
-    const Xb = baseBox.x + (baseBox.width / 2);
-    const Yb = baseBox.y + (baseBox.height / 2);
+    const Xb = baseBox.x + (baseBox.width / 2) ;
+    const Yb = baseBox.y + (baseBox.height / 2) + window.pageYOffset;;
 
     const linearInterpolation = (a, b, p) => (1-p)*a + p*b;
 
     function dotPosition(e) { // e is mouse movement event
-        // Things in this section happen every time mouse moves
 
         // Get mouse position
         let mouseX = e.clientX;
-        let mouseY = e.clientY;
+        let mouseY = e.clientY + window.pageYOffset;;
 
-        // Add drag/prejection/3d effect (amount can be changed in the settings section)
+        // Add drag/projection/3d effect (amount can be changed in the settings section)
         function dotPositionWithProjection(mouse,center) {
             if (mouseX < Xb) {
                 return linearInterpolation(mouse, center, 1-PROJECTION);
@@ -54,7 +52,6 @@ function renderEye(baseEl, dotEl) {
     
         // Calculate distance between center of the eye and the dot
         const d = Math.sqrt( Math.pow( Xd-Xb, 2) + Math.pow( Yd-Yb, 2));
-        //console.log(d);
 
         // Limit the dot within the eye
         let Xr;
@@ -71,17 +68,18 @@ function renderEye(baseEl, dotEl) {
         dotPositionContained();
 
         // Update position of the dot
-        Xr = Xr - dotRadius;
-        Yr = Yr - dotRadius;
-        console.log(Xr);
-        console.log(Yr);
         setTimeout(function(){
-            dotEl.style.left = `${Xr}px`;
-            dotEl.style.top = `${Yr}px`;
+      
+            //how much to move from center
+            const Xm = Xr-Xb;
+            const Ym = Yr-Yb;
+
+            // set new position
+            dotEl.style.transform = `translate(${Xm}px,${Ym}px)`;
 
         }, DELAY); 
     }
 
-    addEventListener("mousemove", (e) => { dotPosition(e); })
+    addEventListener("mousemove", (e) => { dotPosition(e); });
 
 }
