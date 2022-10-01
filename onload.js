ed91540313e68c438c355c2c035c4b4a579cc408
Eva <eva.cerni1@gmail.com>
2022-10-01 13:43:07 +0200

    fix: run eye movement script only on big screen

--- onload.js
+++ onload.js
@@ -2,14 +2,21 @@ window.onload = () => {
 
     console.log("page loaded"); // Things in this section run after the page loaded
 
-    // Initiate eye movement
-    EYE_IDS.forEach(e => {
-        const container = document.getElementById(e);
-        const baseElement = container.querySelector(".eye-base");
-        const dotElement = baseElement.querySelector(".eye-dot");        
-        renderEye(baseElement, dotElement);          
-    });
+    const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
+    console.log(vw);
+
+    // Initiate eye movement (only on screen bigger than 700)
+    if (vw > 700) {
+        EYE_IDS.forEach(e => {
+            const container = document.getElementById(e);
+            const baseElement = container.querySelector(".eye-base");
+            const dotElement = baseElement.querySelector(".eye-dot");        
+            renderEye(baseElement, dotElement);          
+        });
+    }
 
     // Initiate emoji
     randomEmoji();
 }
+
+
